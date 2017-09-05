56c58339f247c7e2699b0c0397bc8c759d539c28
edmlsoncosme <edmilsoncosme@unb.br>
2017-09-05 19:44:05 -0300

    GERANDO O CONFLITO

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="mit" content="2017-08-15T18:43:33-03:00+34377">
-        <title>Curso Work Series - PHP Orientado a Objetos!</title>
+        <title>Curso Work Series - PHP Orientado a Objetos alteando para ver conflito!</title>
 
         <style>
             *{margin: 0; padding: 0; box-sizing: border-box;}
